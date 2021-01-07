b4a22cb45e31ceebdc369653b6014bef47d6c4ee
Kazungu Safari <peter.kazungu.safari@gmail.com>
2021-01-07 17:29:33 +0300

    added Details component and routing using react router

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
+import { Router } from "@reach/router";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle,WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animal({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -19,7 +32,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           <input
             type="text"
